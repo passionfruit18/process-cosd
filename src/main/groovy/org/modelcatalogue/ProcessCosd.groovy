d3ff7bce93f34d7b26f4e23250f8be9baea5efab
James Dai <james.dai.uk@googlemail.com>
2018-03-28 15:51:59 +0100

    Trait Modification rather than Enum Modification

--- src/main/groovy/org/modelcatalogue/ProcessCosd.groovy
+++ src/main/groovy/org/modelcatalogue/ProcessCosd.groovy
@@ -1,5 +1,6 @@
 package org.modelcatalogue
 import com.emmanuelrosa.frostedsheets.*
+import groovy.transform.Immutable
 import org.apache.poi.hssf.usermodel.HSSFCellStyle
 import org.apache.poi.hssf.usermodel.HSSFFont
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
@@ -43,16 +44,38 @@ class ModificationGroup {
     List<Modification> modifications
 }
 
-enum Modification {
-    NOT_LOOKED_AT_SPREADSHEET_YET,
-    MOVE_TO_CORE_PATHOLOGY,
-    // e.g. BR4140 has Data Item Section (DataClass) BREAST - PATHOLOGY which used to have parent data class
-    // Breast but now is in Core.
-    CHANGES_TO_ENUMS,
-    CHANGES_TO_SCHEMA_SPEC,
-    MANUAL,
-    UNCLEAR_FROM_SPREADSHEET
+trait Modification {
+    //abstract void produceGroovyCodeOrModificationInstructions(List<String> ids, List<String> groovyCodeStrings, List<String> modificationInstructions, String dataModelName)
 }
+
+@Immutable
+/**
+ * The data class with the given name will be moved under the 'Core' DataClass.
+ */
+class MoveToCorePathology implements Modification {
+    String dataClassName
+}
+
+class NotLookedAtSpreadsheetYet implements Modification {
+
+}
+
+class ChangesToEnums implements Modification {
+
+}
+
+class ChangesToSchemaSpec implements Modification {
+
+}
+
+class Manual implements Modification {
+
+}
+
+class UnclearFromSpreadsheet implements Modification {
+
+}
+
 
 class ProcessCosd {
     static final String SUBSTANTIAL_CHANGES = 'SUBSTANTIAL CHANGES'
@@ -60,113 +83,113 @@ class ProcessCosd {
 
     static final List<ModificationGroup> COSD_v7_0_6_substantial_and_cosmetic_change_modifications = [
             new ModificationGroup(ids: ['BR4140', 'BR4160', 'BR4170', 'BR4180', 'BR4190', 'BR4200', 'BR4210', 'BR4230', 'BR4220', 'BR4300', 'BR4290', 'BR4280', 'BR4310', 'BR4240', 'BR4250', 'BR4260', 'BR4270'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology('BREAST - PATHOLOGY  ')]),
 
             new ModificationGroup(ids: ['BA3070'],
-                    modifications: [Modification.CHANGES_TO_ENUMS]),
+                    modifications: [new ChangesToEnums()]),
 
             new ModificationGroup(ids: ['BA3150', 'BA3160'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology('CNS - PATHOLOGY ')]),
             new ModificationGroup(ids: ['CO5190', 'CO5210'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology('COLORECTAL - PATHOLOGY')]),
             new ModificationGroup(ids: ['CO5260', 'CO5270', 'CO5280', 'CO5290', 'CO5300'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology('COLORECTAL - PATHOLOGY')]),
 
             new ModificationGroup(ids: ['CT6210'],
-                    modifications: [Modification.MANUAL]),
+                    modifications: [new Manual()]),
 
             new ModificationGroup(ids: ['CT6610', 'CT6620', 'CT6630', 'CT6640', 'CT6650', 'CT6660', 'CT6670'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology('CTYA -  RENAL PATHOLOGY (Paediatric Kidney)')]),
             new ModificationGroup(ids: ['GY7050', 'GY7120', 'GY7130', 'GY7100', 'GY7140', 'GY7190', 'GY7150', 'GY7170', 'GY7180', 'GY7240', 'GY7260', 'GY7270', 'GY7280'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology()]),
 
             new ModificationGroup(ids: ['GY7280'],
-                    modifications: [Modification.CHANGES_TO_ENUMS]),
+                    modifications: [new ChangesToEnums()]),
 
             new ModificationGroup(ids: ['GY7220'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY, Modification.CHANGES_TO_SCHEMA_SPEC]),
+                    modifications: [new MoveToCorePathology(), new ChangesToSchemaSpec()]),
             new ModificationGroup(ids: ['GY7290', 'GY7300', 'GY7350', 'GY7310', 'GY7340', 'GY7360', 'GY7370', 'GY7020', 'GY7060', 'GY7080', 'GY7070', 'GY7090', 'GY7410', 'GY7420', 'GY7230'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology()]),
 
             new ModificationGroup(ids: ['HA8270'],
-                    modifications: [Modification.CHANGES_TO_ENUMS]),
+                    modifications: [new ChangesToEnums()]),
 
             new ModificationGroup(ids: ['HN9300', 'HN9310', 'HN9320', 'HN9330', 'HN9380', 'HN9390', 'HN9400', 'HN9410', 'HN9420', 'HN9430'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology()]),
 
             new ModificationGroup(ids: ['LU10090'],
-                    modifications: [Modification.CHANGES_TO_ENUMS]),
+                    modifications: [new ChangesToEnums()]),
 
             new ModificationGroup(ids: ['LU10100', 'LU10110', 'LU10120', 'LU10130', 'LU10140', 'LU10150', 'LU10160', 'LU10170', 'LU10180'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology()]),
 
             new ModificationGroup(ids: ['SA11120', 'SA11170', 'SA11130', 'SA11140'],
-                    modifications: [Modification.UNCLEAR_FROM_SPREADSHEET]),
+                    modifications: [new UnclearFromSpreadsheet()]),
 
             new ModificationGroup(ids: ['SA11100', 'SA11220'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology()]),
 
             new ModificationGroup(ids: ['SK12450'],
-                    modifications: [Modification.MANUAL]),
+                    modifications: [new Manual()]),
             new ModificationGroup(ids: ['SK12030'],
-                    modifications: [Modification.MANUAL]),
+                    modifications: [new Manual()]),
             new ModificationGroup(ids: ['SK12010'],
-                    modifications: [Modification.MANUAL]),
+                    modifications: [new Manual()]),
 
             new ModificationGroup(ids: ['SK12120'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology()]),
             new ModificationGroup(ids: ['SK12530', 'SK12537', 'SK12650', 'SK12660', 'SK12545', 'SK12565', 'SK12580', 'SK12590', 'SK12600', 'SK12620', 'SK12630', 'SK12430', 'SK12460', 'SK12470', 'SK12480', 'SK12490'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology()]),
             new ModificationGroup(ids: ['UG14470', 'UG14480', 'UG14490'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology()]),
             new ModificationGroup(ids: ['UR15120', 'UR15290', 'UR15130', 'UR15140', 'UR15150', 'UR15160', 'UR15170', 'UR15180', 'UR15190', 'UR15200', 'UR15210', 'UR15220', 'UR15230', 'UR15240', 'UR15250', 'UR15260', 'UR15270', 'UR15310'],
-                    modifications: [Modification.MOVE_TO_CORE_PATHOLOGY]),
+                    modifications: [new MoveToCorePathology()]),
             // END OF SUBSTANTIAL CHANGES
 
             // Cosmetic Changes: Some "realigned", "regrouped"
 
             new ModificationGroup(ids: ['CR0020'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CR3170'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CR0180'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CR3160'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CR0510'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CR2070'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CR3040', 'CR3050'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CT6350', 'CT6750', 'CT6380', 'CT6390', 'CT6450', 'CT6470', 'CT6440', 'CT6220', 'CT6230', 'CT6240'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CT6130', 'CT6140'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CT6710', 'CT6720', 'CT6740', 'CT6770', 'CT6800', 'CT6250', 'CT6270', 'CT6280', 'CT6290', 'CT6330', 'CT6500', 'CT6510', 'CT6590'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CT6560', 'CT6760'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['CT6310', 'CT6360', 'CT6460', 'CT6530', 'CT6550', 'CT6580', 'CT6520'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['HA8660'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['HA8720', 'HA8680', 'HA8280', 'HA8290', 'HA8300', 'HA8310'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['LU10070', 'LU10080'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['SK12030'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['SK12450'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['SK12630'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['UG13100', 'UG13810'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['UG14210', 'UG14230', 'UG13240', 'UG13590', 'UG14290', 'UG13090', 'UG13250', 'UG13070', 'UG13080', 'UG13560', 'UG13580'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
             new ModificationGroup(ids: ['UR15100', 'UR15110'],
-                    modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),
+                    modifications: [new NotLookedAtSpreadsheetYet()]),
 
 
     ]
@@ -363,7 +386,7 @@ class ProcessCosd {
         idSections.each{
             String readableList = "['${it.join("', '")}']"
             println "new ModificationGroup(ids: ${readableList},\n" +
-                    "modifications: [Modification.NOT_LOOKED_AT_SPREADSHEET_YET]),"
+                    "modifications: [new NotLookedAtSpreadsheetYet()]),"
         }
 
         println "Sizes: ${idSections*.size()}"
@@ -417,7 +440,9 @@ class ProcessCosd {
                                    "UG13235", "UG13110", "UG13150",
                                    "UG14190",
                                    "UG13030", "UG14410", "UG13320"],
-        handPickedCompletelyNewItemIds: ['CR6000', 'CR6230', 'CR6400', 'CR6430', 'CR6440', 'CR6450', 'CR6460', 'CR6470', 'CR6100', 'CR6110', 'CR6120', 'CR6130', 'CR6140', 'CR6150', 'CR6160', 'CR6170', 'CR6180', 'CR6190', 'CR6200', 'CR6480', 'CR6300', 'CR6010', 'CR6310', 'CR6220', 'CR6490', 'CR6410', 'CR6420', 'BA3200', 'BA3210', 'CO5400', 'CT6990', 'CT7020', 'CT7200', 'CT7240', 'CT7160', 'CT7170', 'CT7180', 'CT7030', 'CT7400', 'CT7260', 'CT7270', 'CT7380', 'CT7310', 'CT7150', 'CT7070', 'CT7000', 'CT7010', 'CT7110', 'CT7190', 'CT7390', 'CT7370', 'CT7120', 'CT7130', 'CT7140', 'CT7050', 'CT7060', 'CT7040', 'CT7320', 'CT7330', 'CT7340', 'CT7350', 'CT7360', 'CT7080', 'CT7090', 'GY7460', 'GY7450', 'LU10300', 'LU10310', 'LU10340', 'LU10350', 'LU10360', 'LU10420', 'LU10370', 'LU10390', 'SK12710', 'SK12720', 'SK12730', 'SK12740', 'SK12700', 'SK12510', 'UR15400'])
+        handPickedCompletelyNewItemIds: ['CR6000', 'CR6230', 'CR6400', 'CR6430', 'CR6440', 'CR6450', 'CR6460', 'CR6470', 'CR6100', 'CR6110', 'CR6120', 'CR6130', 'CR6140', 'CR6150', 'CR6160', 'CR6170', 'CR6180', 'CR6190', 'CR6200', 'CR6480', 'CR6300', 'CR6010', 'CR6310', 'CR6220', 'CR6490', 'CR6410', 'CR6420', 'BA3200', 'BA3210', 'CO5400', 'CT6990', 'CT7020', 'CT7200', 'CT7240', 'CT7160', 'CT7170', 'CT7180', 'CT7030', 'CT7400', 'CT7260', 'CT7270', 'CT7380', 'CT7310', 'CT7150', 'CT7070', 'CT7000', 'CT7010', 'CT7110', 'CT7190', 'CT7390', 'CT7370', 'CT7120', 'CT7130', 'CT7140', 'CT7050', 'CT7060', 'CT7040', 'CT7320', 'CT7330', 'CT7340', 'CT7350', 'CT7360', 'CT7080', 'CT7090', 'GY7460', 'GY7450', 'LU10300', 'LU10310', 'LU10340', 'LU10350', 'LU10360', 'LU10420', 'LU10370', 'LU10390', 'SK12710', 'SK12720', 'SK12730', 'SK12740', 'SK12700', 'SK12510', 'UR15400']
+        // this is two off the program-found ids because CR6490 is repeated twice.
+        )
 
         processCOSD(cosd_7_0_6_config)
         println "======\n\n======"
